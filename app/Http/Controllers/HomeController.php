4059afd0504826cf56e6fdbcc89f3dc0e13a889c
RSlavov <rumen@eurocoders.com>
2024-01-18 12:20:22 +0200

    move show method to PostController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,27 +32,4 @@ class HomeController extends Controller
 
         return view('home', compact('posts', 'recent_posts', 'categories', 'tags'));
     }
-
-    /**
-     * Show the post.
-     *
-     * @param Post $post
-     * @return View
-     */
-    public function show(Post $post): View
-    {
-        # Get Post with comments
-        $post = Post::with('comments')->findOrFail('slug', $post);
-
-        # Get recent posts
-        $recent_posts = Post::latest()->take(3)->get();
-
-        # Get categories with posts counter
-        $categories = Category::withCount('posts')->take(10)->orderBy('posts_count', 'desc')->get();
-
-        # Get Tags with posts counter
-        $tags = Tag::withCount('posts')->take(10)->orderBy('posts_count', 'desc')->get();
-
-        return view('post', compact('post', 'recent_posts', 'categories', 'tags'));
-    }
 }
